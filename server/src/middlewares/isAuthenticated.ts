bd4a8d37670ff3dab0f6756cd76fbfa29d8e39e0
huniasashraf <hunaisashraf@gmail.com>
2024-08-05 12:07:40 +0530

    auth middleware added

---
+++ server/src/middlewares/isAuthenticated.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Response } from "express";
+import jwt from "jsonwebtoken";
+import { IRequestWithUser } from "../utils/type";
+
+export const isAuthenticated = (
+  req: IRequestWithUser,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      throw new Error("token not found");
+    }
+
+    const user = jwt.verify(token, process.env.JWT_SECRET!) as any;
+    if (!user) {
+      throw new Error("token not found");
+    }
+    req.user = { id: user._id.toString() };
+
+    next();
+  } catch (error) {
+    res.status(403).send({ success: false, message: "User not authorised" });
+  }
+};
